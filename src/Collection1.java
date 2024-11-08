cab64b00dcbee91b26820f4e6420235b6705ee11
rampraveen <rampraveen2710@gmail.com>
2024-11-08 14:41:29 +0530

    First Commit

--- src/Collection1.java
+++ src/Collection1.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Collection1 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>(5);
+    }
 }
